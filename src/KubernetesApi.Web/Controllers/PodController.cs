b0553a98901580cab9f8d30f0acf80d2acc1a85f
Lucas Lobo Leite Pereira <lucas.pereira@telefonica.com>
2022-06-23 11:36:22 -0300

    Start project

---
+++ src/KubernetesApi.Web/Controllers/PodController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using k8s;
+using KubernetesApi.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KubernetesApi.Web.Controllers
+{
+    public class PodController : Controller
+    {
+
+        private IKubernetes _k8s;
+        public PodController(IKubernetes k8s)
+        {
+            this._k8s = k8s;
+        }
+
+        public IActionResult Index([FromQuery] string ns = "default")
+        {
+            var podList = this._k8s.ListNamespacedPod(ns);
+            return View(podList.Items.Select(podItem => new PodViewModel()
+            {
+                Name = podItem.Metadata.Name,
+                Namespace = podItem.Metadata.NamespaceProperty,
+                Containers = podItem.Spec.Containers.Count,
+                Status = podItem.Status.Phase
+            }));
+        }
+    }
+}
---
+++ src/KubernetesApi.Web/Models/PodViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KubernetesApi.Web.Models
+{
+    public class PodViewModel
+    {
+        public string Name { get; set; }
+        public string Namespace { get; set; }
+        public int Containers { get; set; }
+        public string Status { get; set; }
+    }
+}
